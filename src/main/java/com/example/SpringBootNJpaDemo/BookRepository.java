6681d73c162870c04dc807efef12606f1cad876c
Ruturaka2700 <ruturaka2700@gmail.com>
2022-09-29 11:15:47 +0530

    Initial commit

---
+++ src/main/java/com/example/SpringBootNJpaDemo/BookRepository.java
@@ -0,0 +1,10 @@
+package com.example.SpringBootNJpaDemo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface BookRepository extends CrudRepository<Book,Long>{
+
+    List<Book> findByName(String name);
+}
